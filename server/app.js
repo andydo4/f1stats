54e48e85db4a5e402a233640c995b026de431f1b
andydo4 <andrewhuudo@gmail.com>
2025-02-27 12:04:07 -0500

    tweaked for vercel dev

--- server/app.js
+++ server/app.js
@@ -34,5 +34,5 @@ app.use('/api', createProxyMiddleware({
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 });
